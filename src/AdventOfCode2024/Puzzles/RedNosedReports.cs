a0bddf08c09f581f4c7de24f27c7daf4766b86b2
Christian Fischerauer <christian.fischerauer@gmail.com>
2024-12-30 13:15:31 +0100

    feat(day2): solve part 1

--- src/AdventOfCode2024/Puzzles/RedNosedReports.cs
+++ src/AdventOfCode2024/Puzzles/RedNosedReports.cs
@@ -1,19 +1,64 @@
+using System.Reflection;
+using AdventOfCode2024.Helpers;
+
 namespace AdventOfCode2024.Puzzles;
 
 public class RedNosedReports : IPuzzle
 {
-    public Task Prepare()
+    public List<Report> reports = [];
+
+    public async Task Prepare()
     {
-        throw new NotImplementedException();
+        reports = await ParseInput("AdventOfCode2024.Resources.day._2.input");
     }
 
     public Task<string> SolvePart1()
     {
-        throw new NotImplementedException();
+        var result = GetNumberOfSafeReports();
+        return Task.FromResult($"The number of safe reports is {result}");
     }
 
     public Task<string> SolvePart2()
     {
-        throw new NotImplementedException();
+        return Task.FromResult("Not implemented yet");
+    }
+
+    public static async Task<List<Report>> ParseInput(string uri, Assembly? assembly = null)
+    {
+        assembly ??= typeof(HistorianHysteria).Assembly;
+
+        var input = await ResourceHelper.LoadResource(uri, assembly);
+
+        var reports = input.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
+        var result = new List<Report>();
+
+        foreach (var report in reports)
+        {
+            var levels = report.Split(" ").Select(int.Parse).ToList();
+            result.Add(new Report(levels));
+        }
+
+        return result;
+    }
+
+    public int GetNumberOfSafeReports()
+    {
+        int safeReportCount = 0;
+        foreach (var report in reports)
+        {
+            var levels = report.Levels;
+            var distances = levels
+                .Zip(levels.Skip(1))
+                .Select(x => x.Second - x.First);
+            var isSafe = (distances.All(x => x >= 0) || distances.All(x => x <= 0)) && distances.All(x => Math.Abs(x) >= 1 && Math.Abs(x) <= 3);
+            if (isSafe)
+            {
+                safeReportCount++;
+            }
+        }
+        return safeReportCount;
     }
 }
+
+public record Report(List<int> Levels);
--- test/AdventOfCode2024Tests/RedNosedReportsTests.cs
+++ test/AdventOfCode2024Tests/RedNosedReportsTests.cs
@@ -1,10 +1,45 @@
+using AdventOfCode2024.Puzzles;
+
 namespace AdventOfCode2024Tests;
 
 public class RedNosedReportsTests
 {
+    public static readonly List<Report> expectedReports = [
+        new Report([7, 6, 4, 2, 1]),
+        new Report([1, 2, 7, 8, 9]),
+        new Report([9, 7, 6, 2, 1]),
+        new Report([1, 3, 2, 4, 5]),
+        new Report([8, 6, 4, 4, 1]),
+        new Report([1, 3, 6, 7, 9])
+    ];
+
     [Fact]
     public async Task ParseInputTest()
     {
-        throw new NotImplementedException();
+        // Arrange
+        var uri = "AdventOfCode2024Tests.Resources.day._2.input";
+
+        // Act
+        var reports = await RedNosedReports.ParseInput(uri, typeof(RedNosedReportsTests).Assembly);
+
+        // Assert
+        expectedReports
+            .Zip(reports)
+            .ToList()
+            .ForEach(x =>
+            {
+                Assert.Equal(x.First.Levels, x.Second.Levels);
+            });
+    }
+
+    [Fact]
+    public void GetNumberOfSafeReportsTest()
+    {
+        var redNosedReports = new RedNosedReports() { reports = expectedReports };
+        // Act
+        var result = redNosedReports.GetNumberOfSafeReports();
+
+        // Assert
+        Assert.Equal(2, result);
     }
 }
